a62633bdf0f1fd8cf70e0b501888065df78bb2a1
Matthew Leung <mdleung@gmail.com>
2018-07-22 20:39:04 -0700

    Test BrainStation
    test

---
+++ assignment-1.js
@@ -0,0 +1,13 @@
+
+var numbers = [];
+
+for (var i=0; i < 10; i++){
+    numbers.push(i);
+}
+/* your code here */
+console.log(numbers[0])
+console.log(numbers[9])
+
+var car = {};
+
+car.colour = "red";
